48c431535503a01168dac04adc4788dbbbf41428
NisreenMaher <43481978+NisreenMaher@users.noreply.github.com>
2019-05-03 02:10:32 +0300

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,13 @@ Route::get('/posts','PostsController@index')->name('posts.index');
 
 Route::get('/posts/create','PostsController@create')->name('posts.create');
 Route::post('/posts','PostsController@store')->name('posts.store');
+
 Route::get('/posts/{id}','PostsController@show')->name('posts.show');
 
 Route::get('/posts/{id}/edit','PostsController@edit')->name('posts.edit');
 Route::put('/posts/{id}','PostsController@update')->name('posts.update');
 
 Route::delete('/posts/{id}','PostsController@destroy')->name('posts.destroy');
-
-
-// Route::get('/posts/{id}/{author}', function ($id, $author) {
-//     return "The post with id ". $id . ' has author name ' . $author ;
-// });
-
-
-
 
 
 
